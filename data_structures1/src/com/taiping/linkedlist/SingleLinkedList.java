1813ff059c2134dc4eaf4ed298f605ed49d5e475
chenlianywy <chenlianywy@163.com>
2022-12-14 11:17:46 +0800

    Initial commit

---
+++ data_structures1/src/com/taiping/linkedlist/SingleLinkedList.java
@@ -0,0 +1,13 @@
+package com.taiping.linkedlist;
+
+/**
+ * @author 10105
+ * 单链表
+ */
+public class SingleLinkedList {
+    public static void main(String[] args) {
+        System.out.println("asd");
+    }
+}
+
+
